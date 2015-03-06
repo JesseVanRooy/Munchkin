b424382fe7f88ed7ce572d703255a61a5c7f5910
JesseVanRooy <jesse.vanrooy@student.kdg.be>
2015-03-06 14:17:43 +0100

    -x- Jesse Committed Files -x-

--- src/model/ArmorSet.java
+++ src/model/ArmorSet.java
@@ -1,7 +1,12 @@
 package model;
 
+import model.kaarten.treasurekaarten.ArmorKaart;
+
+import java.util.List;
+
 /**
  * Created by Jesse on 6/03/2015.
  */
 public class ArmorSet {
+    private ArmorKaart helm, chestPlate, pants, boots;
 }
