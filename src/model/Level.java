6780678829c0c151d88dea1d4cbbd6216094f61e
JesseVanRooy <jesse.vanrooy@student.kdg.be>
2015-03-06 23:06:30 +0100

    -x- Jesse Committed Files -x-

---
+++ src/model/Level.java
@@ -0,0 +1,8 @@
+package model;
+
+/**
+ * Created by Jesse on 6/03/2015.
+ */
+public class Level {
+
+}
--- src/model/Speler.java
+++ src/model/Speler.java
@@ -4,6 +4,7 @@ import model.kaarten.deurkaarten.KlasseKaart;
 import model.kaarten.deurkaarten.RasKaart;
 import model.kaarten.treasurekaarten.WapenKaart;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -16,4 +17,58 @@ public class Speler {
     private RasKaart rasKaart;
     private ArmorSet armorSet;
     private WapenSet wapenSet;
+
+    public Speler (){
+        this(new ArrayList<Kaart>());
+    }
+
+    public Speler (List<Kaart> rugzak){
+        this.level = 1;
+        this.rugzak = rugzak;
+        this.armorSet = new ArmorSet();
+        this.wapenSet = new WapenSet();
+
+    }
+
+    /* level methodes */
+
+    // OPM : aparte klasse?
+
+    public void gaLevelOmhoog(){
+        this.level++;
+    }
+
+    public void gaLevelOmlaag(){
+        this.level--;
+    }
+
+    public void resetLevel(){
+        this.level = 1;
+    }
+
+    public boolean is10(){
+        return this.level==10;
+    }
+
+    public int getLevel(){
+        return this.level;
+    }
+
+    /* rugzak methodes */
+
+
+
+    /* klasseKaart methodes */
+
+
+
+    /* rasKaart methodes */
+
+
+
+    /* armorSet methodes */
+
+
+
+    /* wapenSet methodes */
 }
